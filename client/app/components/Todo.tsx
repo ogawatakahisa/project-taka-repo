f2206ccb33de7fc44578c59222c904cdcd4b6595
ogawatakahisa <takahisa.0717.39@gmail.com>
2025-01-31 15:59:37 +0900

    first commit

---
+++ client/app/components/Todo.tsx
@@ -0,0 +1,103 @@
+import Rafce, { useState } from "react";
+import { TodoType } from "../types"
+import useSWR from "swr";
+
+type TodoProps = {
+    todo: TodoType;
+}
+
+async function fetcher(key: string) {
+    // .then=非同期処理の結果を受け取る
+    // 指定したエンドポイ　ントから.thenで値を受け取り
+    // json形式で返す
+    return fetch(key).then((res) => res.json());
+}
+
+const Todo = ({ todo }: TodoProps) => {
+    // ISEditingにstateの状態が入る
+    // setIsEdtingは関数で、IsEditingの状態を変える
+    const [isEditing, setIsEditing] = useState(false);
+    // 編集ボタンを押した時にinputに書かれている文字列を残す
+    // もともと入っていた値editedTitleに新しく入力されたsetEditedTitleを入れる
+    const [editedTitle, setEditedTitle] = useState<string>(todo.title);
+
+    // useSWR＝APIのデータ取得（フェッチ）を簡単にできる
+    // &関数コンポーネントでstateを管理できる
+    const { data, isLoading, error, mutate } = useSWR(
+        "http://localhost:8080/allTodos",
+        // 引数ないように見えるけど、useSWRの仕組みで自動でつけてくれてる
+        // この時の引数はuseSWRで呼び出したAPIのurlが渡される
+        fetcher
+    );
+
+    const handleEdit = async() => {
+        // ONとOFを切り替える
+        // setIsEditing(!IsEditing);　// 非推奨
+        setIsEditing((prev) => !prev); //関数型の更新
+        // この時のisEditingは変更前のisEditingの値を見ている
+        // →useStateは次のレンダリング時に反映される仕組みになっている
+        // mutate があるおかげで際レンダリングされる
+        if (isEditing) {
+            const response = await fetch(`http://localhost:8080/editTodo/${todo.id}`, {
+                method: "PUT",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ title: editedTitle })
+            });
+
+            if (response.ok) {
+                const editieTodo = await response.json();
+                //SWRのキャッシュを更新する 
+                // キャッシュが新しいdataに変わる＝再レンダリングされる
+                mutate([...data, editieTodo]); 
+                setEditedTitle("")
+            }
+        }
+    }
+
+
+
+    return (
+    <div>
+        <li className="py-4">
+        <div className="flex items-center justify-between">
+            <div className="flex items-center">
+            <input
+                id="todo1"
+                name="todo1"
+                type="checkbox"
+                className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"
+            />
+            <label className="ml-3 block text-gray-900">
+                {isEditing ? (
+                    <input
+                        type="text"
+                        className="border rounded py-1 px2"
+                        value={editedTitle} 
+                        // eとは、イベントオブジェクトで、ユーザが行った操作に関する情報を持つ
+                        // e.targetはイベントが発生した要素を指す
+                        onChange={(e) => setEditedTitle(e.target.value)}
+                    />              
+                ): (
+                    <span className="text-lg font-medium mr-2">{todo.title}</span>
+                )}
+            </label>
+            </div>
+
+          {/* 編集・削除ボタン */}
+            <div className="flex items-center space-x-2">
+                <button onClick={handleEdit}
+                className="duration-150 bg-green-600 hover:bg-green-700 text-white font-medium py-1 px-2 rounded">
+                
+                {isEditing ? "Save": "✒"}            
+            </button>
+            <button className="bg-red-500 hover:bg-red-600 text-white font-medium py-1 px-2 rounded">
+                ✖
+            </button>
+            </div>
+        </div>
+        </li>
+    </div>
+    );
+};
+
+export default Todo;
---
+++ client/app/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+import Image from "next/image";
+import Todo from "./components/Todo";
+import useSWR from "swr";
+import { features } from "process";
+
+import { useEffect, useRef } from 'react';
+import { todo } from "node:test";
+import { TodoType } from "./types";
+import { useRouter } from "next/navigation";
+
+// useSWRフックから呼び出される
+async function fetcher(key: string) {
+  // .then=非同期処理の結果を受け取る
+  // 指定したエンドポイ　ントから.thenで値を受け取り
+  // json形式で返す
+  return fetch(key).then((res) => res.json());
+}
+
+// コンポーネント（UIを一部の独立した再利用可能な部品としてカプセル化したもの）の定義
+// この中でuseSWRを使用しているため、これがマウントされるとfetcherが実行される
+// →マウント＝コンポーネントが画面上に初めて出現することを意味する
+// fetcherから受け取った結果をdataに入れる
+export default function Home() {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const router = useRouter();
+  
+  // const allTodos = await fetch("API", {cache: "force-cache"}); //SSR
+  // keyには、エンドポンとを指定。server側はポート＝8080
+  const { data, isLoading, error, mutate } = useSWR(
+    "http://localhost:8080/allTodos",
+    fetcher
+  );
+
+  // console.log("取得後情報")
+  // console.log(data)
+  const handlesubmit = async(e: React.FormEvent) => {
+    console.log("新しいタスクAA")
+    e.preventDefault()//ページリロードを止める
+
+    // //入力した内容が取得できているのかを確認するための処理だったからコメントアウトした
+    // if (inputRef.current) {
+    //   console.log("新しいタスク")
+    //   console.log(inputRef.current?.value)
+    // }
+
+    const response = await fetch(`http://localhost:8080/createTodo`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: inputRef.current?.value,
+        isCompleted: false
+      })
+    });
+
+    router.refresh();
+    if (response.ok) {
+      const newTodo = await response.json();
+      mutate([...data, newTodo]);
+      inputRef.current!.value = "";
+    }
+
+  }
+
+
+  return (
+    <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-32 py-4 px-4">
+      <div className="px-4 py-2">
+        <h1 className="text-gray-800 font-bold text-2xl uppercase">To-Do List</h1>
+      </div>
+
+      {/* タスク追加フォーム */}
+      <form className="w-full max-w-sm mx-auto px-4 py-2" onSubmit={ handlesubmit}>{/* ユーザがフォームを送信するとき */}
+        <div className="flex items-center border-b-2 border-teal-500 py-2">
+          <input
+            className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+            type="text"
+            placeholder="Add a task"
+            ref={inputRef}
+          />
+          <button
+            className="duration-150 flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
+      </form>
+
+      {/* タスクリスト */}
+      <ul className="divide-y divide-gray-200 px-4">
+        {data?.map((todo: TodoType) => (
+          // todoは配列で、dataを意味する
+          // データ取得apiからreturnさるときのdata
+
+          // keyはMapを使うときに必要なもの
+          //todo={todo}はTodoコンポーネントにtodoというpropsを渡している
+          // 左辺がTodoコンポーネントを表る
+          // todoにはtodo全体を入れて渡すって感じ
+          // Reactでは、keyは特別な属性として扱われているため、子コンポーネントには渡らない
+          // 親、子コンポーネントについてだけどexport default function Home() {が親コンポーネント
+          // この下の行で小コンポーネントを呼び出している感じ
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
